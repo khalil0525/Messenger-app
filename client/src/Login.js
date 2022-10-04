8cc79e0bf4db5cbb5a419f95434741342b71ce11
khalil0525 <collinskhalil@hotmail.com>
2022-10-04 13:09:51 -0400

    Added new styles to login

--- client/src/Login.js
+++ client/src/Login.js
@@ -7,10 +7,65 @@ import {
   Button,
   FormControl,
   TextField,
+  makeStyles,
 } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  headerContainer: {
+    display: "flex",
+    alignItems: "flex-start",
+    paddingTop: "23px",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  headerInner: {
+    padding: "8px",
+    alignSelf: "center",
+    alignItems: "center",
+  },
+  headerContent: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+  },
+  contentContainer: {
+    display: "flex",
+    margin: "64px 32px",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  text: {
+    color: "#b0b0b0",
+    textAlign: "center",
+    fontWeight: "400",
+    whiteSpace: "nowrap",
+    marginRight: "21px",
+  },
+  formBox: {
+    display: "flex",
+    margin: "64px 32px",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  welcomeText: {
+    color: "#000000",
+    fontSize: "26px",
+    fontWeight: "600",
+    paddingBottom: "20px",
+  },
+}));
 
 const Login = ({ user, login }) => {
   const history = useHistory();
+  const classes = useStyles();
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -27,41 +82,55 @@ const Login = ({ user, login }) => {
   }, [user, history]);
 
   return (
-    <Grid container justifyContent="center">
+    <Grid container justifyContent="center" className={classes.root}>
       <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Link href="/register" to="/register">
-            <Button>Register</Button>
-          </Link>
-        </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                />
-              </FormControl>
-            </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
+        <Box container item className={classes.headerContainer}>
+          <Box className={classes.headerInner}>
+            <Box className={classes.headerContent}>
+              <Typography variant="body1" className={classes.text}>
+                Need to register?
+              </Typography>
+              <Link href="/register" to="/register">
+                <Button>Register</Button>
+              </Link>
+            </Box>
+          </Box>
+        </Box>
+        <Box className={classes.contentContainer}>
+          <Grid container justifyContent="center" direction="column">
+            <Typography variant="body1" className={classes.welcomeText}>
+              Welcome Back!
+            </Typography>
+
+            <form onSubmit={handleLogin}>
+              <Grid>
+                <Grid>
+                  <FormControl margin="normal" required>
+                    <TextField
+                      aria-label="username"
+                      label="Username"
+                      name="username"
+                      type="text"
+                    />
+                  </FormControl>
+                </Grid>
+                <FormControl margin="normal" required>
+                  <TextField
+                    label="password"
+                    aria-label="password"
+                    type="password"
+                    name="password"
+                  />
+                </FormControl>
+                <Grid>
+                  <Button type="submit" variant="contained" size="large">
+                    Login
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
           </Grid>
-        </form>
+        </Box>
       </Box>
     </Grid>
   );
