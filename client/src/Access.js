7fd59e6f7b2c4627e8cebdc00ebfb261ac46240c
khalil0525 <collinskhalil@hotmail.com>
2022-10-04 13:09:39 -0400

    Created access component

---
+++ client/src/Access.js
@@ -0,0 +1,55 @@
+import { Box, Grid, makeStyles, Paper, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  bgGrid: {
+    backgroundSize: "cover",
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  },
+  bgBox: {
+    display: "flex",
+    minHeight: "100vh",
+    alignItems: "center",
+    flexDirection: "column",
+
+    backgroundSize: "cover",
+    justifyContent: "center",
+    backgroundImage:
+      "linear-gradient(180deg, rgb(58,141,255, 0.75) 0%, rgb(134,185,255, 0.75) 100%)",
+    backgroundPosition: "center",
+  },
+  text: {
+    color: "#ffffff",
+    maxWidth: "300px",
+    marginTop: "30px",
+    textAlign: "center",
+  },
+}));
+
+const Access = ({ WrappedComponent }) => {
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.root}>
+      <Grid item sm={4} md={7} className={classes.bgGrid}>
+        <Box className={classes.bgBox}>
+          <img
+            width="67"
+            src="https://res.cloudinary.com/dna1o7hrm/image/upload/v1612406581/r1hhd9ry5wxux0tqhju0.png"
+            alt="chat bubble"
+          />
+          <Typography variant="h4" className={classes.text}>
+            Welcome to ChatApp
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={1} sm={8} md={5} elevation6>
+        {WrappedComponent}
+      </Grid>
+    </Grid>
+  );
+};
+export default Access;
