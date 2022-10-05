8740504e5fb0db2240ef779e9c4d0265a490c622
khalil0525 <collinskhalil@hotmail.com>
2022-10-05 15:03:21 -0400

    updated socket information

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 import React from "react";
 
-export const socket = io(window.location.origin);
+export const socket = io("https://kc-chat-app-api.herokuapp.com/");
 socket.on("connect", () => {
   console.log("connected to server");
 });
