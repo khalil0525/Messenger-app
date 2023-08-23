93f17e62832294a85a539874691a3a6d03e6f07b
Khalil Collins <Collinskhalil@hotmail.com>
2023-08-23 19:12:42 -0400

    changes

--- server/db/db.js
+++ server/db/db.js
@@ -1,29 +1,19 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const db =
-  process.env.ENV === "production"
-    ? new Sequelize({
-        database: process.env.DB_DATABASE,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: "postgres",
-        dialectOptions: {
-          ssl: {
-            require: true, // This will help you. But you will see nwe error
-            rejectUnauthorized: false, // This line will fix new error
-          },
-        },
-      })
-    : new Sequelize({
-        database: process.env.DB_DATABASE,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: "postgres",
-      });
+const db = new Sequelize({
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
+    },
+  },
+});
 
 module.exports = db;
