0ebd70eea004cdd574c504bd2235276b848cb437
Khalil Collins <Collinskhalil@hotmail.com>
2023-08-28 19:17:14 -0400

    Css changes

--- client/src/components/ActiveChat/OtherUserBubble.js
+++ client/src/components/ActiveChat/OtherUserBubble.js
@@ -3,54 +3,78 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Box, Typography, Avatar } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
-  root: {
-    display: "flex",
-  },
-  avatar: {
-    height: 30,
-    width: 30,
-    marginRight: 11,
-    marginTop: 6,
-  },
-  usernameDate: {
-    fontSize: 11,
-    color: "#BECCE2",
-    fontWeight: "bold",
-    marginBottom: 5,
-  },
-  bubble: {
-    backgroundImage: "linear-gradient(225deg, #6CC1FF 0%, #3A8DFF 100%)",
-    borderRadius: "0 10px 10px 10px",
-  },
-  text: {
-    fontSize: 14,
-    fontWeight: "bold",
-    color: "#FFFFFF",
-    letterSpacing: -0.2,
-    padding: 8,
-  },
+	root: {
+		display: "flex",
+		marginBottom: 10,
+	},
+	avatar: {
+		height: 40,
+		width: 40,
+		marginRight: 11,
+		alignSelf: "flex-end",
+	},
+
+	username: {
+		fontSize: 11,
+		color: "rgba(79, 127, 183, 100%)",
+		fontWeight: "bold",
+		marginBottom: 5,
+	},
+	date: {
+		fontSize: 11,
+		color: "rgba(255, 255, 255, 50%)",
+		fontWeight: "bold",
+		marginBottom: 5,
+	},
+	bubble: {
+		backgroundColor: "rgba(33, 35, 38, 60%)",
+		borderRadius: "8px 10px 10px 2px",
+		padding: 6,
+		paddingLeft: 10,
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "start",
+		alignItems: "start",
+		minWidth: "140px",
+	},
+	bubbleHeader: {
+		display: "flex",
+		justifyContent: "space-between",
+		width: "100%",
+	},
+	text: {
+		fontSize: 14,
+		fontWeight: "bold",
+		color: "#FFFFFF",
+		letterSpacing: -0.2,
+	},
 }));
 
 const OtherUserBubble = ({ text, time, otherUser }) => {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <Box className={classes.root}>
-      <Avatar
-        alt={otherUser.username}
-        src={otherUser.photoUrl}
-        className={classes.avatar}
-      />
-      <Box>
-        <Typography className={classes.usernameDate}>
-          {otherUser.username} {time}
-        </Typography>
-        <Box className={classes.bubble}>
-          <Typography className={classes.text}>{text}</Typography>
-        </Box>
-      </Box>
-    </Box>
-  );
+	return (
+		<Box className={classes.root}>
+			<Avatar
+				alt={otherUser.username}
+				src={otherUser.photoUrl}
+				className={classes.avatar}
+			/>
+			<Box>
+				<Box className={classes.bubble}>
+					<Box className={classes.bubbleHeader}>
+						{" "}
+						<Typography className={classes.username}>
+							{otherUser.username}
+						</Typography>
+						<Typography className={classes.date}>{time}</Typography>
+					</Box>
+
+					<Typography className={classes.text}>{text}</Typography>
+				</Box>
+			</Box>
+		</Box>
+	);
 };
 
 export default OtherUserBubble;
--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -3,53 +3,98 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Box, Typography, Avatar } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-end",
-  },
-  date: {
-    fontSize: 11,
-    color: "#BECCE2",
-    fontWeight: "bold",
-    marginBottom: 5,
-  },
-  text: {
-    fontSize: 14,
-    color: "#91A3C0",
-    letterSpacing: -0.2,
-    padding: 8,
-    fontWeight: "bold",
-  },
-  bubble: {
-    background: "#F4F6FA",
-    borderRadius: "10px 10px 0 10px",
-  },
-  readAvatar: {
-    height: 20,
-    width: 20,
-    marginTop: 9,
-  },
+	root: {
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "flex-end",
+		marginBottom: 10,
+	},
+	contentContainer: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "flex-end",
+	},
+	avatar: {
+		height: 40,
+		width: 40,
+		marginLeft: 10,
+	},
+
+	username: {
+		fontSize: 11,
+		color: "rgba(79, 127, 183, 100%)",
+		fontWeight: "bold",
+		marginBottom: 5,
+	},
+	date: {
+		fontSize: 11,
+		color: "rgba(255, 255, 255, 50%)",
+		fontWeight: "bold",
+		marginBottom: 5,
+	},
+	bubble: {
+		background: "rgba(66, 104, 149, 42%)",
+		borderRadius: "10px 10px 0 10px",
+		padding: 6,
+		paddingLeft: 10,
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "start",
+		alignItems: "start",
+		minWidth: "140px",
+	},
+	bubbleHeader: {
+		display: "flex",
+		justifyContent: "space-between",
+		width: "100%",
+	},
+	text: {
+		fontSize: 14,
+		fontWeight: "bold",
+		color: "#FFFFFF",
+		letterSpacing: -0.2,
+	},
+
+	readAvatar: {
+		height: 20,
+		width: 20,
+		marginTop: 9,
+	},
 }));
 
-const SenderBubble = ({ time, text, otherUser, isLastReadMessage }) => {
-  const classes = useStyles();
+const SenderBubble = ({ time, text, otherUser, isLastReadMessage, user }) => {
+	const classes = useStyles();
 
-  return (
-    <Box className={classes.root}>
-      <Typography className={classes.date}>{time}</Typography>
-      <Box className={classes.bubble}>
-        <Typography className={classes.text}>{text}</Typography>
-      </Box>
-      {isLastReadMessage && (
-        <Avatar
-          alt={otherUser.username}
-          src={otherUser.photoUrl}
-          className={classes.readAvatar}
-        />
-      )}
-    </Box>
-  );
+	return (
+		<Box className={classes.root}>
+			<Box className={classes.contentContainer}>
+				<Box className={classes.bubble}>
+					<Box className={classes.bubbleHeader}>
+						{" "}
+						<Typography className={classes.username}>
+							{user.username}
+						</Typography>
+						<Typography className={classes.date}>{time}</Typography>
+					</Box>
+
+					<Typography className={classes.text}>{text}</Typography>
+				</Box>
+				{isLastReadMessage && (
+					<Avatar
+						alt={otherUser.username}
+						src={otherUser.photoUrl}
+						className={classes.readAvatar}
+					/>
+				)}
+			</Box>
+			<Avatar
+				alt={otherUser.username}
+				src={user.photoUrl}
+				className={classes.avatar}
+			/>
+		</Box>
+	);
 };
 
 export default SenderBubble;
