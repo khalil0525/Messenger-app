36877d22efa3edcdb2a80a87a1964eea20f200b4
Khalil Collins <Collinskhalil@hotmail.com>
2023-11-03 02:22:54 -0400

    changed db connection

--- db/db.js
+++ db/db.js
@@ -1,22 +1,37 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const db = new Sequelize({
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: "postgres",
-  dialectOptions:
-    process.env.env === "development"
-      ? null
-      : {
-          ssl: {
-            require: true, // This will help you. But you will see nwe error
-            rejectUnauthorized: false, // This line will fix new error
-          },
-        },
-});
+// const db = new Sequelize({
+//   database: process.env.DB_DATABASE,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   dialect: "postgres",
+//   dialectOptions:
+//     process.env.env === "development"
+//       ? null
+//       : {
+//           ssl: {
+//             require: true, // This will help you. But you will see nwe error
+//             rejectUnauthorized: false, // This line will fix new error
+//           },
+//         },
+// });
 
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+db.authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 module.exports = db;
