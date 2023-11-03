62a3bb9d52a3708372780e2e5afa7ff61fcd28d6
Khalil Collins <Collinskhalil@hotmail.com>
2023-11-03 01:34:01 -0400

    disabled upload photo

--- middleware/uploadImage.js
+++ middleware/uploadImage.js
@@ -1,51 +1,52 @@
-const { S3Client } = require("@aws-sdk/client-s3");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
-const path = require("path");
-const s3 = new S3Client({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
-// S3
-function sanitizeFile(file, cb) {
-  // Define the allowed extension
-  const fileExts = [".png", ".jpg", ".jpeg", ".gif"];
+// const { S3Client } = require("@aws-sdk/client-s3");
+// const multer = require("multer");
+// const multerS3 = require("multer-s3");
+// const path = require("path");
+// // const s3 = new S3Client({
+// //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+// //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+// //   region: process.env.AWS_REGION,
+// // });
+// // S3
+// const s3 = null;
+// function sanitizeFile(file, cb) {
+//   // Define the allowed extension
+//   const fileExts = [".png", ".jpg", ".jpeg", ".gif"];
 
-  // Check allowed extensions
-  const isAllowedExt = fileExts.includes(
-    path.extname(file.originalname.toLowerCase())
-  );
+//   // Check allowed extensions
+//   const isAllowedExt = fileExts.includes(
+//     path.extname(file.originalname.toLowerCase())
+//   );
 
-  // Mime type must be an image
-  const isAllowedMimeType = file.mimetype.startsWith("image/");
+//   // Mime type must be an image
+//   const isAllowedMimeType = file.mimetype.startsWith("image/");
 
-  if (isAllowedExt && isAllowedMimeType) {
-    return cb(null, true); // no errors
-  } else {
-    // pass error msg to callback, which can be displaye in frontend
-    cb("Error: File type not allowed!");
-  }
-}
-// Set up multer with S3 storage
-const s3Storage = multerS3({
-  s3,
-  bucket: process.env.S3_BUCKET,
-  acl: "public-read",
-  metadata: function (req, file, cb) {
-    cb(null, { fieldName: file.fieldname });
-  },
-  key: function (req, file, cb) {
-    cb(null, `user_photos/${Date.now().toString()}_${file.originalname}`);
-  },
-});
-const uploadImage = multer({
-  storage: s3Storage,
-  fileFilter: (req, file, callback) => {
-    sanitizeFile(file, callback);
-  },
-  limits: {
-    fileSize: 1024 * 1024 * 2, // 2mb file size
-  },
-});
-module.exports = { uploadImage, s3 };
+//   if (isAllowedExt && isAllowedMimeType) {
+//     return cb(null, true); // no errors
+//   } else {
+//     // pass error msg to callback, which can be displaye in frontend
+//     cb("Error: File type not allowed!");
+//   }
+// }
+// // Set up multer with S3 storage
+// const s3Storage = multerS3({
+//   s3,
+//   bucket: process.env.S3_BUCKET,
+//   acl: "public-read",
+//   metadata: function (req, file, cb) {
+//     cb(null, { fieldName: file.fieldname });
+//   },
+//   key: function (req, file, cb) {
+//     cb(null, `user_photos/${Date.now().toString()}_${file.originalname}`);
+//   },
+// });
+// const uploadImage = multer({
+//   storage: s3Storage,
+//   fileFilter: (req, file, callback) => {
+//     sanitizeFile(file, callback);
+//   },
+//   limits: {
+//     fileSize: 1024 * 1024 * 2, // 2mb file size
+//   },
+// });
+// module.exports = { uploadImage, s3 };
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,41 +1,41 @@
 const router = require("express").Router();
 const User = require("../../db/models/user");
 
-const { uploadImage, s3 } = require("../../middleware/uploadImage");
-router.post(
-  "/update-photo",
-  uploadImage.single("image"),
-  async (req, res, next) => {
-    try {
-      const photoUrl = req.file.location; // Get the S3 URL of the uploaded photo
+// const { uploadImage, s3 } = require("../../middleware/uploadImage");
+// router.post(
+//   "/update-photo",
+//   uploadImage.single("image"),
+//   async (req, res, next) => {
+//     try {
+//       const photoUrl = req.file.location; // Get the S3 URL of the uploaded photo
 
-      if (!req.user) {
-        return res.status(404).json({ error: "User not found" });
-      }
+//       if (!req.user) {
+//         return res.status(404).json({ error: "User not found" });
+//       }
 
-      let data = {};
-      if (req.file) {
-        data.image = req.file.location;
-      }
-      // If user already has a photoUrl, delete the old photo from S3
-      if (req.user.photoUrl) {
-        const oldPhotoKey = req.user.photoUrl.split("/").pop();
-        await s3
-          .deleteObject({
-            Bucket: process.env.S3_BUCKET_NAME,
-            Key: `user_photos/${oldPhotoKey}`,
-          })
-          .promise();
-      }
+//       let data = {};
+//       if (req.file) {
+//         data.image = req.file.location;
+//       }
+//       // If user already has a photoUrl, delete the old photo from S3
+//       if (req.user.photoUrl) {
+//         const oldPhotoKey = req.user.photoUrl.split("/").pop();
+//         await s3
+//           .deleteObject({
+//             Bucket: process.env.S3_BUCKET_NAME,
+//             Key: `user_photos/${oldPhotoKey}`,
+//           })
+//           .promise();
+//       }
 
-      req.user.photoUrl = photoUrl;
-      await req.user.save();
+//       req.user.photoUrl = photoUrl;
+//       await req.user.save();
 
-      res.json(req.user);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+//       res.json(req.user);
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
 
 module.exports = router;
